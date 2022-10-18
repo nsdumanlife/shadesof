cd42e123b685f52764a42a3f2b162771fb0acc7e
NSDuman <dumanlife@yandex.com>
2022-10-18 16:17:43 +0300

    Create function to return an array of n shades

---
+++ index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-plusplus */
+function shadesOfGrey(n) {
+  if (n <= 0) return []
+
+  const shades = ['#010101']
+
+  for (let i = 1; i < Math.min(n, 254); i++) {
+    let temp = '010101'
+    temp = (parseInt(temp, 16) + i * 0x010101).toString(16)
+
+    if (temp.length < 6) temp = `0${temp}`
+
+    shades.push(`#${temp}`)
+  }
+  return shades
+}
+
+module.exports = shadesOfGrey
